7aeb603c19445318cb5ca4b114f49c3865255ed1
Connor Smith <cjs.smith1993@gmail.com>
2015-04-07 14:40:00 -0600

    Lab 11

---
+++ Labs/Lab11/cluster/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	console.log("ID " + process.pid);
+  res.render('index', { title: 'Cluster', pid:process.pid });
+});
+
+router.get('/pid', function(req, res, next) {
+	console.log('Service ID ' + process.pid);
+	for (var i = 0; i < 10000; i++) {
+
+	}
+	res.json({
+		title: 'Cluster',
+		pid: process.pid
+	});
+});
+
+module.exports = router;
